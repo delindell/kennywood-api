5b9329215ac8f3ddf7d99d3fdf8595e56d7073ae
Davis Lindell <daviselindell@gmail.com>
2020-08-18 15:59:43 -0500

    setup

---
+++ kennywood/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from kennywoodapi.models import *
+from kennywoodapi.views import register_user, login_user
+from kennywoodapi.views import ParkAreas, Attractions, ItineraryView
+
+# pylint: disable=invalid-name
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'parkareas', ParkAreas, 'parkarea')
+router.register(r'attractions', Attractions, 'attraction')
+router.register(r'itineraryitems', ItineraryView, 'itinerary')
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/', register_user),
+    path('login/', login_user),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
---
+++ kennywoodapi/views/__init__.py
@@ -0,0 +1,5 @@
+from .register import register_user
+from .register import login_user
+from .parkarea import ParkAreas
+from .attraction import Attractions
+from .itinerary import ItineraryView
